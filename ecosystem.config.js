c3d90cf659f3e97dfcc694b36152f2280744eea4
josiascastro <josiascastro2003@gmail.com>
2023-10-18 15:51:17 -0300

    change port

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: 'JosiasCastroApp',
-      port: '3003',
+      port: '3005',
       exec_mode: 'cluster',
       instances: 'max',
       script: './.output/server/index.mjs'
